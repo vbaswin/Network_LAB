a7af7d15c6395c18a0053ba0cf3b9848bfe0c625
Aswin V B <aswinvb10a@gmail.com>
2023-06-03 16:16:02 +0530

    Delete Stop_And_Wait directory

--- Stop_And_Wait/chumma.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main() {
-	char *msg = "hi I am Aswin", *msg2 = (char *)malloc(sizeof(char));
-	strcpy(msg2, msg);
-	printf("%s\n", msg2);
-}
